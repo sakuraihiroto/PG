8bd82a2c03908d00ebb07e70caba35ecaf31c87f
hirotosakurai <k021g1130@g.neec.ac.jp>
2023-02-06 19:46:07 +0900

    03-03_課題内容

--- Enemy.h
+++ Enemy.h
@@ -1,14 +1,26 @@
 #pragma once
-#include <stdio.h>
-#include <array>
 
 class Enemy
 {
-private:
-	static bool isAlive_;
-
 public:
-	void SetAlive(bool isExist);
-	void Print();
+	Enemy();
+	~Enemy();
+
+	//行動
+	void Approach();
+	void Shoot();
+	void GetAway();
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw();
+
+private:
+	static void (Enemy::* pFuncTable[])();
+
+	//行動フェーズ
+	int phase_;
 
 };
